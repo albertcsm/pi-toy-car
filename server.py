568ee59fd73de2ba46ed5d62ec8426bc8f92905f
Albert Cheung <albertcsm@gmail.com>
2018-10-20 01:37:38 +0800

    add motion control

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from MotorDriver import MotorDriver
 Motor = MotorDriver()
 
-from flask import Flask, send_from_directory, redirect
+from flask import Flask, send_from_directory, redirect, request
 app = Flask(__name__)
 
-speed = 100
+import json
+
+MAX_SPEED = 100
 
 @app.route('/')
 def serve_root():
@@ -16,27 +18,55 @@ def serve_static(path):
 
 @app.route("/action/left", methods=['POST'])
 def action_left():
-    Motor.MotorRun(0, 'forward', speed)
-    Motor.MotorRun(1, 'backward', speed)
+    Motor.MotorRun(0, 'forward', MAX_SPEED)
+    Motor.MotorRun(1, 'backward', MAX_SPEED)
     return "left"
 
 @app.route("/action/forward", methods=['POST'])
 def action_forward():
-    Motor.MotorRun(0, 'forward', speed)
-    Motor.MotorRun(1, 'forward', speed)
+    Motor.MotorRun(0, 'forward', MAX_SPEED)
+    Motor.MotorRun(1, 'forward', MAX_SPEED)
     return "forward"
 
 @app.route("/action/back", methods=['POST'])
 def action_back():
-    Motor.MotorRun(0, 'backward', speed)
-    Motor.MotorRun(1, 'backward', speed)
+    Motor.MotorRun(0, 'backward', MAX_SPEED)
+    Motor.MotorRun(1, 'backward', MAX_SPEED)
     return "back"
 
 @app.route("/action/right", methods=['POST'])
 def action_right():
-    Motor.MotorRun(0, 'backward', speed)
-    Motor.MotorRun(1, 'forward', speed)
+    Motor.MotorRun(0, 'backward', MAX_SPEED)
+    Motor.MotorRun(1, 'forward', MAX_SPEED)
     return "right"
+
+@app.route("/action/move", methods=['POST'])
+def action_move():
+    instruction = json.loads(request.data)
+
+    speed0 = 0
+    speed1 = 0
+
+    if instruction['steering'] < 0:
+        speed1 -= MAX_SPEED * 2 * abs(instruction['steering'])
+    else:
+        speed0 -= MAX_SPEED * 2 * abs(instruction['steering'])
+
+    basicSpeed = MAX_SPEED * abs(instruction['forward']) - (speed0 + speed1) / 2
+
+    direction = instruction['forward'] / abs(instruction['forward'])
+    speed0 = (speed0 + basicSpeed) * direction
+    speed1 = (speed1 + basicSpeed) * direction
+
+    if speed0 >= 0:
+        Motor.MotorRun(0, 'forward', speed0)
+    else:
+        Motor.MotorRun(0, 'backward', -speed0)
+    if speed1 >= 0:
+        Motor.MotorRun(1, 'forward', speed1)
+    else:
+        Motor.MotorRun(1, 'backward', -speed1)
+    return "move"
 
 @app.route("/action/stop", methods=['POST'])
 def action_stop():
