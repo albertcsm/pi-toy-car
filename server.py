f4f8b3babda47492841b8603275ce1a5193b4984
Albert Cheung <albertcsm@gmail.com>
2018-11-01 01:39:12 +0800

    tune speed

--- server.py
+++ server.py
@@ -48,24 +48,24 @@ def action_move():
     speed1 = 0
 
     if instruction['steering'] < 0:
-        speed1 -= MAX_SPEED * 2 * abs(instruction['steering'])
+        speed1 -= MAX_SPEED * 1.5 * abs(instruction['steering'])
     else:
-        speed0 -= MAX_SPEED * 2 * abs(instruction['steering'])
+        speed0 -= MAX_SPEED * 1.5 * abs(instruction['steering'])
 
-    basicSpeed = MAX_SPEED * abs(instruction['forward']) - (speed0 + speed1) / 2
+    basicSpeed = MAX_SPEED * 1.5 * abs(instruction['forward']) - (speed0 + speed1) / 2
 
     direction = instruction['forward'] / abs(instruction['forward'])
     speed0 = (speed0 + basicSpeed) * direction
     speed1 = (speed1 + basicSpeed) * direction
 
     if speed0 >= 0:
-        Motor.MotorRun(0, 'forward', speed0)
+        Motor.MotorRun(0, 'forward', min(speed0, MAX_SPEED))
     else:
-        Motor.MotorRun(0, 'backward', -speed0)
+        Motor.MotorRun(0, 'backward', min(-speed0, MAX_SPEED))
     if speed1 >= 0:
-        Motor.MotorRun(1, 'forward', speed1)
+        Motor.MotorRun(1, 'forward', min(speed1, MAX_SPEED))
     else:
-        Motor.MotorRun(1, 'backward', -speed1)
+        Motor.MotorRun(1, 'backward', min(-speed1, MAX_SPEED))
     return "move"
 
 @app.route("/action/stop", methods=['POST'])
