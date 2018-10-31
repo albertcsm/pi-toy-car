9b3624313cec11ef2e5e4809af65cd66611d33a3
Albert Cheung <albertcsm@gmail.com>
2018-11-01 02:38:53 +0800

    modulize controller frontend

--- server.py
+++ server.py
@@ -1,78 +1,64 @@
+import time
+
 from MotorDriver import MotorDriver
 Motor = MotorDriver()
 
-from flask import Flask, send_from_directory, redirect, request
-app = Flask(__name__)
-
-import json
-
 MAX_SPEED = 100
 
-@app.route('/')
-def serve_root():
-    return redirect("/ui/index.html", code=302)
+class CarController():
 
-@app.route('/ui/<path:path>')
-def serve_static(path):
-    return send_from_directory('ui', path)
+    def action_left(self):
+        Motor.MotorRun(0, 'forward', MAX_SPEED)
+        Motor.MotorRun(1, 'backward', MAX_SPEED)
 
-@app.route("/action/left", methods=['POST'])
-def action_left():
-    Motor.MotorRun(0, 'forward', MAX_SPEED)
-    Motor.MotorRun(1, 'backward', MAX_SPEED)
-    return "left"
+    def action_forward(self):
+        Motor.MotorRun(0, 'forward', MAX_SPEED)
+        Motor.MotorRun(1, 'forward', MAX_SPEED)
 
-@app.route("/action/forward", methods=['POST'])
-def action_forward():
-    Motor.MotorRun(0, 'forward', MAX_SPEED)
-    Motor.MotorRun(1, 'forward', MAX_SPEED)
-    return "forward"
+    def action_back(self):
+        Motor.MotorRun(0, 'backward', MAX_SPEED)
+        Motor.MotorRun(1, 'backward', MAX_SPEED)
 
-@app.route("/action/back", methods=['POST'])
-def action_back():
-    Motor.MotorRun(0, 'backward', MAX_SPEED)
-    Motor.MotorRun(1, 'backward', MAX_SPEED)
-    return "back"
+    def action_right(self):
+        Motor.MotorRun(0, 'backward', MAX_SPEED)
+        Motor.MotorRun(1, 'forward', MAX_SPEED)
 
-@app.route("/action/right", methods=['POST'])
-def action_right():
-    Motor.MotorRun(0, 'backward', MAX_SPEED)
-    Motor.MotorRun(1, 'forward', MAX_SPEED)
-    return "right"
+    def action_move(self, instruction):
+        speed0 = 0
+        speed1 = 0
 
-@app.route("/action/move", methods=['POST'])
-def action_move():
-    instruction = json.loads(request.data)
+        if instruction['steering'] < 0:
+            speed1 -= MAX_SPEED * 1.5 * abs(instruction['steering'])
+        else:
+            speed0 -= MAX_SPEED * 1.5 * abs(instruction['steering'])
 
-    speed0 = 0
-    speed1 = 0
+        basicSpeed = MAX_SPEED * 1.5 * abs(instruction['forward']) - (speed0 + speed1) / 2
 
-    if instruction['steering'] < 0:
-        speed1 -= MAX_SPEED * 1.5 * abs(instruction['steering'])
-    else:
-        speed0 -= MAX_SPEED * 1.5 * abs(instruction['steering'])
+        direction = instruction['forward'] / abs(instruction['forward'])
+        speed0 = (speed0 + basicSpeed) * direction
+        speed1 = (speed1 + basicSpeed) * direction
 
-    basicSpeed = MAX_SPEED * 1.5 * abs(instruction['forward']) - (speed0 + speed1) / 2
+        if speed0 >= 0:
+            Motor.MotorRun(0, 'forward', min(speed0, MAX_SPEED))
+        else:
+            Motor.MotorRun(0, 'backward', min(-speed0, MAX_SPEED))
+        if speed1 >= 0:
+            Motor.MotorRun(1, 'forward', min(speed1, MAX_SPEED))
+        else:
+            Motor.MotorRun(1, 'backward', min(-speed1, MAX_SPEED))
 
-    direction = instruction['forward'] / abs(instruction['forward'])
-    speed0 = (speed0 + basicSpeed) * direction
-    speed1 = (speed1 + basicSpeed) * direction
+    def action_stop(self):
+        Motor.MotorRun(0, 'forward', 0)
+        Motor.MotorRun(1, 'forward', 0)
 
-    if speed0 >= 0:
-        Motor.MotorRun(0, 'forward', min(speed0, MAX_SPEED))
-    else:
-        Motor.MotorRun(0, 'backward', min(-speed0, MAX_SPEED))
-    if speed1 >= 0:
-        Motor.MotorRun(1, 'forward', min(speed1, MAX_SPEED))
-    else:
-        Motor.MotorRun(1, 'backward', min(-speed1, MAX_SPEED))
-    return "move"
-
-@app.route("/action/stop", methods=['POST'])
-def action_stop():
-    Motor.MotorRun(0, 'forward', 0)
-    Motor.MotorRun(1, 'forward', 0)
-    return "right"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    car_controller = CarController()
+
+    from webfrontend import WebFrontend
+    web_frontend = WebFrontend(car_controller)
+    web_frontend.start()
+
+    print('Started')
+    while True:
+        time.sleep(1)
---
+++ webfrontend.py
@@ -0,0 +1,55 @@
+import json
+
+from flask import Flask, send_from_directory, redirect, request
+app = Flask(__name__)
+car_controller = None
+
+
+@app.route('/')
+def serve_root():
+    return redirect("/ui/index.html", code=302)
+
+@app.route('/ui/<path:path>')
+def serve_static(path):
+    return send_from_directory('ui', path)
+
+@app.route("/action/left", methods=['POST'])
+def action_left():
+    car_controller.action_left()
+    return "left"
+
+@app.route("/action/forward", methods=['POST'])
+def action_forward():
+    car_controller.action_forward()
+    return "forward"
+
+@app.route("/action/back", methods=['POST'])
+def action_back():
+    car_controller.action_back()
+    return "back"
+
+@app.route("/action/right", methods=['POST'])
+def action_right():
+    car_controller.action_right()
+    return "right"
+
+@app.route("/action/move", methods=['POST'])
+def action_move():
+    instruction = json.loads(request.data)
+    car_controller.action_move(instruction)
+    return "move"
+
+@app.route("/action/stop", methods=['POST'])
+def action_stop():
+    car_controller.action_stop()
+    return "right"
+
+
+class WebFrontend():
+
+    def __init__(self, controller):
+        global car_controller
+        car_controller = controller
+
+    def start(self):
+        app.run(host="0.0.0.0")
